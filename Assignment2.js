7e46e3153ce5b86678e8dea5a894e7079ad69596
yashwadatkar-coditas <158143580+yashwadatkar-coditas@users.noreply.github.com>
2024-01-30 16:33:12 +0530

    Add files via upload

---
+++ Assignment2.js
@@ -0,0 +1,38 @@
+//Declare four variables without assigning values
+let variableOne
+let variableTwo
+let variableThree
+let variableFour
+
+// Declare four variables with assigned values
+let name = 'yash'
+let city = 'pune'
+let animal = 'Dog'
+let color = 'red'
+
+
+
+
+
+
+
+
+
+
+//Declare variables to store your first name, last name, marital status, country and age in multiple lines
+
+
+let fatherName 
+let motherName 
+let fatherAge
+
+
+//Declare variables to store your first name, last name, marital status, country and age in a single line
+let firstName , lastName , martialStatus , country , age;
+
+//Declare two variables myAge and yourAge and assign them initial values and log to the browser console.
+let myAge = 21
+let yourAge = 20
+
+console.log(myAge)
+console.log(yourAge)
