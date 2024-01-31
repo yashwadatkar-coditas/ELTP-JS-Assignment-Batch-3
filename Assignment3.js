90f794d282f8f35a6bf39cf11f807bf9f62bb105
yashwadatkar-coditas <158143580+yashwadatkar-coditas@users.noreply.github.com>
2024-01-31 12:59:14 +0530

    Add files via upload
    Assignment 3

---
+++ Assignment3.js
@@ -0,0 +1,12 @@
+//Create a file and declare variables and assign string, boolean, undefined and null data types
+const myName = 'Yash';
+const isIntern = true;
+const jobRole = undefined;
+const phoneNumber = null;
+
+ // Create file and use the JavaScript typeof operator to check different data types. Check the data type of each variable
+
+console.log(typeof myName);
+console.log(typeof isIntern);
+console.log(typeof jobRole);
+console.log(typeof phoneNumber);
